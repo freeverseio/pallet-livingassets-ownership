7b0765d4bd26fa107aadc4e4986905db635271fe
Dastan Samatov <dastan@freeverse.io>
2023-07-03 18:21:35 +0300

    Clippy fixes

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -3,18 +3,18 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as LivingAssetsOwnership;
 use frame_benchmarking::v1::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+    create_collection {
+        let s in 0 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+    }: _(RawOrigin::Signed(caller.clone()), s as u64)
+    verify {
+        assert_eq!(OwnerOfCollection::<T>::get(s as u64), Some(caller));
+    }
 
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+    impl_benchmark_test_suite!(LivingAssetsOwnership, crate::mock::new_test_ext(), crate::mock::Test);
 }
